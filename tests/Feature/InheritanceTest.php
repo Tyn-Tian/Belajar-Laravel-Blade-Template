e8228b25e219dc4463bb26925f3e3506683896e5
Christian <kristian2015ok@gmail.com>
2024-05-26 22:43:02 +0700

    Add: Template Inheritance

---
+++ tests/Feature/InheritanceTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class InheritanceTest extends TestCase
+{
+    public function testInheritance()
+    {
+        $this->view('child', [])
+            ->assertSeeText("Nama Aplikasi - Halaman Utama")
+            ->assertSeeText("Default Header")
+            ->assertSeeText("Deskripsi Header")
+            ->assertDontSeeText("Default Content")
+            ->assertSeeText("Ini adalah halaman utama");
+
+        $this->view('child-default', [])
+            ->assertSeeText("Nama Aplikasi - Halaman Utama")
+            ->assertSeeText("Default Header")
+            ->assertSeeText("Default Content")
+            ->assertDontSeeText("Deskirpsi Header")
+            ->assertDontSeeText("Ini adalah halaman utama");
+    }
+}
