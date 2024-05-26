c45ab4a4e7477dfed8a642a65691f6ac45010e22
Christian <kristian2015ok@gmail.com>
2024-05-26 15:39:20 +0700

    Add: Menampilkan View

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return view('hello', ['name' => 'Laravel']);
+});
+
+Route::get('/world', function () {
+    return view('hello.world', ['name' => 'Laravel']);
+});
